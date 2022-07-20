ddb5fee9aebb3c25bf72690ccb833409db0bc7e2
anguyen-22 <95489593+anguyen-22@users.noreply.github.com>
2022-07-20 00:01:23 -0400

    Add files via upload

---
+++ AI Project/src/SimpleAI.java
@@ -0,0 +1,21 @@
+public class SimpleAI extends ConnectFourPlayer {
+	
+	public SimpleAI(){
+	    return;
+    }
+	
+	public int getMove(int [][] board, int player){
+        int[] moves = ConnectFourBoard.getMoves(board);
+        for (int i = 0; i < moves.length; i++) {
+        	int[][] newBoard = ConnectFourBoard.forecastMove(board, moves[i], player);
+        	
+        	if (ConnectFourBoard.hasWinner(newBoard) == player) {
+        		return moves[i];
+        	}
+        		
+        }
+        int m = (int) (Math.random() * moves.length);
+        return moves[m];
+    }
+
+}
